0b49ddf8a1ae46ef19525b17204de3bfaa0d1006
Arihant Bansal <17180950+arihantbansal@users.noreply.github.com>
2021-11-23 16:26:36 +0530

    Add roadmap

--- src/pages/AboutUs/index.js
+++ src/pages/AboutUs/index.js
@@ -1,4 +1,7 @@
 import teamAvatar from "assets/team_avatar.svg";
+import twitterIcon from "assets/twitter_icon.svg";
+import discordIcon from "assets/discord_icon.svg";
+import roadmap from "assets/roadmap.svg";
 
 const AboutUs = ({}) => {
 	const team = [
@@ -7,32 +10,32 @@ const AboutUs = ({}) => {
 			title: "Lead Artist and Illustrator",
 		},
 		{
-			name: "Leochii",
-			title: "Lead Artist and Illustrator",
+			name: "Kaija",
+			title: "Lead Art Supervisor",
 		},
 		{
-			name: "Leochii",
-			title: "Lead Artist and Illustrator",
+			name: "Sirsapient",
+			title: "Community Manager",
 		},
 		{
-			name: "Leochii",
-			title: "Lead Artist and Illustrator",
+			name: "Globey",
+			title: "Content and Branding",
 		},
 		{
-			name: "Leochii",
-			title: "Lead Artist and Illustrator",
+			name: "Flu",
+			title: "Marketing",
 		},
 		{
-			name: "Leochii",
-			title: "Lead Artist and Illustrator",
+			name: "Miss Freya",
+			title: "Lead Marketing",
 		},
 		{
-			name: "Leochii",
-			title: "Lead Artist and Illustrator",
+			name: "Tailchakra",
+			title: "Technical Supervisor",
 		},
 		{
-			name: "Leochii",
-			title: "Lead Artist and Illustrator",
+			name: "Ray",
+			title: "Web Design",
 		},
 	];
 
@@ -55,6 +58,68 @@ const AboutUs = ({}) => {
 							<p>{item.title}</p>
 						</div>
 					))}
+				</div>
+
+				<div className="font-semibold text-3xl mt-24 mb-12">Roadmap</div>
+				<div className="flex flex-row justify-center mb-12">
+					<div className="flex flex-col justify-between">
+						<div className="mt-8">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua.
+						</div>
+						<div></div>
+						<div className="mb-16">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua.
+						</div>
+						<div></div>
+					</div>
+					<img src={roadmap} />
+					<div className="flex flex-col-reverse justify-between">
+						<div className="mb-11">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua.
+						</div>
+						<div></div>
+						<div className="mt-11">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua.
+						</div>
+						<div></div>
+					</div>
+				</div>
+				<div className="mb-12">
+					<p className="text-center">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+						ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+						aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+						labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+						exercitation ullamco laboris nisi ut aliquip ex ea co
+					</p>
+				</div>
+				<div className="text-3xl my-8 font-semibold">Join our Community</div>
+				<div className="mb-10 text-lg">
+					stay tuned for more news each day, giveaways and more!
+				</div>
+				<div className="flex flex-row justify-center space-x-12 text-center justify-items-center mb-20">
+					<div className="flex flex-col justify-center">
+						<img src={twitterIcon} alt="Twitter Icon" className="h-10" />
+						<div>
+							<a href="#" className="underline text-gray-300">
+								Follow us on Twitter
+							</a>
+						</div>
+					</div>
+					<div className="flex flex-col justify-center">
+						<img src={discordIcon} alt="Discord Icon" className="h-10" />
+						<div>
+							<a href="#" className="underline text-gray-300">
+								Join our Discord here
+							</a>
+						</div>
+					</div>
 				</div>
 			</div>
 		</>
