969777fa76a57308644cb796a73c231bac974ba4
Arihant Bansal <17180950+arihantbansal@users.noreply.github.com>
2021-11-24 12:02:28 +0530

    Chart trial

--- src/pages/CollectionView/SaleHistory.js
+++ src/pages/CollectionView/SaleHistory.js
@@ -1,5 +1,15 @@
 import { Dialog, Transition } from "@headlessui/react";
 import { Fragment, useState } from "react";
+import {
+	LineChart,
+	Line,
+	XAxis,
+	YAxis,
+	CartesianGrid,
+	Tooltip,
+	Legend,
+	ResponsiveContainer,
+} from "recharts";
 import DarkDropdownWithIcon from "components/Dropdowns/DarkDropdown/icon";
 import SaleIcon from "assets/sale_icon.svg";
 import LinkIconDark from "assets/copy_link_icon.svg";
@@ -51,6 +61,51 @@ const TableRow = () => {
 		</tr>
 	);
 };
+
+const data = [
+	{
+		name: "Page A",
+		uv: 4000,
+		pv: 2400,
+		amt: 2400,
+	},
+	{
+		name: "Page B",
+		uv: 3000,
+		pv: 1398,
+		amt: 2210,
+	},
+	{
+		name: "Page C",
+		uv: 2000,
+		pv: 9800,
+		amt: 2290,
+	},
+	{
+		name: "Page D",
+		uv: 2780,
+		pv: 3908,
+		amt: 2000,
+	},
+	{
+		name: "Page E",
+		uv: 1890,
+		pv: 4800,
+		amt: 2181,
+	},
+	{
+		name: "Page F",
+		uv: 2390,
+		pv: 3800,
+		amt: 2500,
+	},
+	{
+		name: "Page G",
+		uv: 3490,
+		pv: 4300,
+		amt: 2100,
+	},
+];
 
 const SaleHistory = () => {
 	const [open, setOpen] = useState(true);
@@ -128,6 +183,35 @@ const SaleHistory = () => {
 									<div className="font-semibold">47382 ONE</div>
 								</div>
 							</div>
+
+							{/* <div>
+								<ResponsiveContainer width="1000px" height="1000px">
+									<LineChart
+										width={500}
+										height={300}
+										data={data}
+										margin={{
+											top: 5,
+											right: 30,
+											left: 20,
+											bottom: 5,
+										}}>
+										<CartesianGrid strokeDasharray="3 3" />
+										<XAxis dataKey="name" stroke="#0096c7" />
+										<YAxis />
+										<Tooltip />
+										<Legend />
+										<Line
+											type="monotone"
+											dataKey="pv"
+											stroke="#8884d8"
+											activeDot={{ r: 8 }}
+										/>
+										<Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+									</LineChart>
+								</ResponsiveContainer>
+							</div> */}
+
 							<div className="my-8 rounded-md py-4">
 								<table className="table-auto w-full border-collapse border border-gray-300">
 									<thead className="font-bold text-center h-16">
