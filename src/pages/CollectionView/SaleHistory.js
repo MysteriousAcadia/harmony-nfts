8f8c00925708e7fb234c24dc262baba490ce266d
Arihant Bansal <17180950+arihantbansal@users.noreply.github.com>
2021-11-25 21:21:15 +0530

    Fix warning

--- src/pages/CollectionView/SaleHistory.js
+++ src/pages/CollectionView/SaleHistory.js
@@ -76,44 +76,6 @@ const SaleRow = ({ data = {} }) => {
 	);
 };
 
-const data = [
-	{
-		name: "Page A",
-		pv: 2400,
-		amt: 2400,
-	},
-	{
-		name: "Page B",
-		pv: 1398,
-		amt: 2210,
-	},
-	{
-		name: "Page C",
-		pv: 9800,
-		amt: 2290,
-	},
-	{
-		name: "Page D",
-		pv: 3908,
-		amt: 2000,
-	},
-	{
-		name: "Page E",
-		pv: 4800,
-		amt: 2181,
-	},
-	{
-		name: "Page F",
-		pv: 3800,
-		amt: 2500,
-	},
-	{
-		name: "Page G",
-		pv: 4300,
-		amt: 2100,
-	},
-];
-
 const SaleHistory = ({ historyOpen, setHistoryOpen }) => {
 	const [saleData, setSaleData] = useState();
 	const [selectedTime, setSelectedTime] = useState(0);
@@ -294,7 +256,6 @@ sales(where:{timestamp_gt:${selectedTime}}, orderBy:timestamp, orderDirection:de
 											type="monotone"
 											dataKey="value"
 											stroke="rgb(17, 92, 112)"
-											activeDot={{ r: 8 }}
 										/>
 									</LineChart>
 								</div>
